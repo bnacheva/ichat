ca43804618b30baf6c5a744804d8a564c40cdcb6
bnacheva <bori97@abv.bg>
2019-12-10 21:01:12 +0200

    Adding the connection controller

--- backend/src/main/java/chat/app/server/controller/ConnectionController.java
+++ backend/src/main/java/chat/app/server/controller/ConnectionController.java
@@ -1,4 +1,60 @@
 package chat.app.server.controller;
 
+import chat.app.server.exception.UsernameAlreadyUsedException;
+import chat.app.server.model.User;
+import chat.app.server.repository.UserRepository;
+import chat.app.server.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ConnectionController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userDao;
+
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> login(@RequestBody User user) {
+
+        try {
+            User connectedUser = userService.connect(user);
+            template.convertAndSend("/channel/login", connectedUser);
+        } catch (UsernameAlreadyUsedException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        }
+
+        return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void logout(@RequestBody User user) {
+        User disconnectedUser = userService.disconnect(user);
+        template.convertAndSend("/channel/logout", disconnectedUser);
+    }
+
+    @RequestMapping(value = "/listUsers", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<User> findConnectedUsers() {
+        return userDao.findAll();
+    }
+
+    @RequestMapping(value = "/clearAll", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void clearAll() {
+        userDao.deleteAll();
+    }
 }
